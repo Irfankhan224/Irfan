432cf8717f7fc2f0968cdc30219a51ac82464a36
Irfankhan224 <151997474+Irfankhan224@users.noreply.github.com>
2024-01-07 15:25:43 +0430

    Create Irfan.py
    git clone https://github.com/Irfankhan224/Irfan.git
    ls
    cd irfan-khan
    ls
    python Irfan.py

---
+++ Irfan.py
@@ -0,0 +1,36 @@
+## COMMAND :
+
+[![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&pause=1000&color=FF2C10&background=31FF9400&width=435&lines=Old+FB+id+Cloning+Tool+Enjoy+Guys%F0%9F%A4%9F)](https://git.io/typing-svg)
+
+* `pkg update && pkg upgrade`
+
+* `pkg install python`
+
+* `pkg install git`
+
+* `pip install requests`
+
+* `pip install mechanize`
+
+* `pip install futures`
+
+* `pip install rich`
+
+* `pip install bs4`
+
+* `rm -rf OLD-FB-CRACK`
+
+* `git clone https://github.com/MUMIT-404-CYBER/OLD-FB-CRACK`
+
+* `cd OLD-FB-CRACK`
+
+* `python Crack.py`
+
+
+___This Tool is Free Enjoy Dear User.___</br>
+
+## SCREENSHOT :
+<br>
+<p align="center">
+<img src="__scr__/old-fb.jpg"/>
+</p>
